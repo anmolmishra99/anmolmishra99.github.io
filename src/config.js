c234e517e9e55a6ba8758cf944707fb9443084b9
Anmol Mishra <49812717+anmolmishra99@users.noreply.github.com>
2022-02-24 23:27:46 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ module.exports = {
         sortBy: 'updated', // stars | updated
         limit: 8, // How many projects to display.
         exclude: {
-            forks: false, // Forked projects will not be displayed if set to true.
-            projects: ['fav'] // These projects will not be displayed. example: ['my-project1', 'my-project2']
+            forks: true, // Forked projects will not be displayed if set to true.
+            projects: ['Subu'] // These projects will not be displayed. example: ['my-project1', 'my-project2']
         }
     },
     social: {
